20ddabcc7386f47ecad5424f32183eceb2af0bb3
Calvin Szeto <szeto.calvin@gmail.com>
2014-03-12 18:16:11 -0500

    Add AJAX request for nearest locations.

--- app/app.py
+++ app/app.py
@@ -1,5 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from locator import Locator
 app = Flask(__name__)
+
+@app.route('/_locate_nearest')
+def locate_nearest():
+    address = request.args.get('address', 'Austin, TX', type=str)
+    locator = Locator("/home/puppyplus/Projects/eater/data/geocoded_restaurants.json") 
+    return jsonify({"results":[location[0]["name"] for location in locator.nearest(address)]})
 
 @app.route('/')
 def index():
--- app/static/locator.js
+++ app/static/locator.js
@@ -1,2 +1,11 @@
 google.maps.event.addDomListener(window, 'load', initializeMap);
-
+$(function() {
+	$('a#locate').bind('click', function() {
+		$.getJSON($SCRIPT_ROOT + '/_locate_nearest', {
+			address: $('input[name="address"]').val(),
+		}, function(data) {
+			$("#result").text(data.results);
+		});
+		return false;
+	});
+});
