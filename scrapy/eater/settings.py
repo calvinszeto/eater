ef0ec30f984e20918e83f0d70f09bf2f69e27483
Calvin Szeto <szeto.calvin@gmail.com>
2014-03-14 23:00:18 -0500

    Update scrapy to put data in postgres database.

--- scrapy/eater/settings.py
+++ scrapy/eater/settings.py
@@ -11,5 +11,13 @@ BOT_NAME = 'eater'
 SPIDER_MODULES = ['eater.spiders']
 NEWSPIDER_MODULE = 'eater.spiders'
 
+ITEM_PIPELINES = ['eater.pipelines.EaterPipeline']
+
+DATABASE = {'drivername': 'postgres',
+            'host': 'localhost',
+            'port': '5432',
+            'username': 'postgres',
+            'password': 'okokok',
+            'database': 'eater'}
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'eater (+http://www.yourdomain.com)'
--- scrapy/eater/spiders/list_spider.py
+++ scrapy/eater/spiders/list_spider.py
@@ -7,13 +7,13 @@ class ListSpider(Spider):
     name = "list"
     allowed_domains = ["eater.com"]
     start_urls = [
-        "http://localhost:8050/render.html?url=http://houston.eater.com/archives/2014/01/07/the-essential-38-houston-restaurants-january-2014-1.php&timeout=10&wait=0.5"
+        "http://localhost:8050/render.html?url=http://houston.eater.com/archives/2014/01/07/the-essential-38-houston-restaurants-january-2014-1.php&timeout=3&wait=0.5"
     ]
 
     def parse(self, response):
         sel = Selector(response)
         restaurants = sel.xpath('//div[@class="point-mode point-mode-list"]/div/div')
-        items = []
+        # items = []
         for restaurant in restaurants:
             item = RestaurantItem()
             name = restaurant.xpath('div[@class="name fn org overflow-controlled"]/@title').extract()
@@ -22,5 +22,6 @@ class ListSpider(Spider):
             item['name'] = name[0] if name else ""
             item['address'] = address[0] if address else ""
             item['website'] = website[0] if website else ""
-            items.append(item)
-        return items
+            yield item
+       #     items.append(item)
+       # return items
