8b6d6cd98b89a51fc94cc447393e141c611cd0bd
Calvin Szeto <szeto.calvin@gmail.com>
2014-06-11 01:05:06 -0500

    Add json_combine for combining multiple json lists.

--- scrapy/eater/spiders/list_spider.py
+++ scrapy/eater/spiders/list_spider.py
@@ -8,11 +8,11 @@ class ListSpider(Spider):
     allowed_domains = ["eater.com"]
     start_urls = [
         # Houston
-        #"http://localhost:8050/render.html?url=http://houston.eater.com/archives/2014/01/07/the-essential-38-houston-restaurants-january-2014-1.php&timeout=5&wait=1",
-        #"http://localhost:8050/render.html?url=http://houston.eater.com/archives/2014/04/08/the-essential-38-houston-restaurants-april-2014.php&timeout=5&wait=1",
-        ## New York
-        #"http://localhost:8050/render.html?url=http://ny.eater.com/archives/2014/01/the_38_essential_new_york_restaurants_january_14.php&timeout=5&wait=1",
-        #"http://localhost:8050/render.html?url=http://ny.eater.com/archives/2014/04/the_38_essential_new_york_restaurants_april_14.php&timeout=5&wait=1",
+        "http://localhost:8050/render.html?url=http://houston.eater.com/archives/2014/01/07/the-essential-38-houston-restaurants-january-2014-1.php&timeout=5&wait=1",
+        "http://localhost:8050/render.html?url=http://houston.eater.com/archives/2014/04/08/the-essential-38-houston-restaurants-april-2014.php&timeout=5&wait=1",
+        # New York
+        "http://localhost:8050/render.html?url=http://ny.eater.com/archives/2014/01/the_38_essential_new_york_restaurants_january_14.php&timeout=5&wait=1",
+        "http://localhost:8050/render.html?url=http://ny.eater.com/archives/2014/04/the_38_essential_new_york_restaurants_april_14.php&timeout=5&wait=1",
         # Austin
         "http://localhost:8050/render.html?url=http://austin.eater.com/archives/2014/01/07/the-38-essential-austin-restaurants-january-14.php&timeout=5&wait=1",
         "http://localhost:8050/render.html?url=http://austin.eater.com/archives/2014/04/08/the-38-essential-austin-restaurants-april-14.php&timeout=5&wait=1",
