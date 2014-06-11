411ece8591d39fd473024dc8d0e90e1070644607
Calvin Szeto <szeto.calvin@gmail.com>
2014-06-10 23:00:58 -0500

    Update spider with more links.

--- scrapy/eater/spiders/list_spider.py
+++ scrapy/eater/spiders/list_spider.py
@@ -7,13 +7,20 @@ class ListSpider(Spider):
     name = "list"
     allowed_domains = ["eater.com"]
     start_urls = [
-        "http://localhost:8050/render.html?url=http://houston.eater.com/archives/2014/01/07/the-essential-38-houston-restaurants-january-2014-1.php&timeout=3&wait=0.5"
+        # Houston
+        #"http://localhost:8050/render.html?url=http://houston.eater.com/archives/2014/01/07/the-essential-38-houston-restaurants-january-2014-1.php&timeout=5&wait=1",
+        #"http://localhost:8050/render.html?url=http://houston.eater.com/archives/2014/04/08/the-essential-38-houston-restaurants-april-2014.php&timeout=5&wait=1",
+        ## New York
+        #"http://localhost:8050/render.html?url=http://ny.eater.com/archives/2014/01/the_38_essential_new_york_restaurants_january_14.php&timeout=5&wait=1",
+        #"http://localhost:8050/render.html?url=http://ny.eater.com/archives/2014/04/the_38_essential_new_york_restaurants_april_14.php&timeout=5&wait=1",
+        # Austin
+        "http://localhost:8050/render.html?url=http://austin.eater.com/archives/2014/01/07/the-38-essential-austin-restaurants-january-14.php&timeout=5&wait=1",
+        "http://localhost:8050/render.html?url=http://austin.eater.com/archives/2014/04/08/the-38-essential-austin-restaurants-april-14.php&timeout=5&wait=1",
     ]
 
     def parse(self, response):
         sel = Selector(response)
         restaurants = sel.xpath('//div[@class="point-mode point-mode-list"]/div/div')
-        # items = []
         for restaurant in restaurants:
             item = RestaurantItem()
             name = restaurant.xpath('div[@class="name fn org overflow-controlled"]/@title').extract()
@@ -23,5 +30,3 @@ class ListSpider(Spider):
             item['address'] = address[0] if address else ""
             item['website'] = website[0] if website else ""
             yield item
-       #     items.append(item)
-       # return items
